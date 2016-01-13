4f327f6481e8b87934fb02961c4b9737b279f414
Jusqu123 <jusquu123@gmail.com>
2016-01-13 15:25:28 +0200

    karkausvuositehtävä

---
+++ harjoitus7/Program.cs
@@ -0,0 +1,32 @@
+﻿
+//Tee ohjelma, joka näyttää onko annettu vuosi karkausvuosi. Vuosiluku kysytään käyttäjältä. 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harjoitus7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vuosi;
+            Console.Write("vuosi");
+            vuosi = int.Parse(Console.ReadLine());
+            Console.WriteLine("vuotesi   " + vuosi);
+            if (vuosi % 4 == 0 && vuosi % 100 != 0 || vuosi % 400 == 0)
+            {
+
+                Console.WriteLine("karkausvuosi");
+
+            }
+            else
+                Console.WriteLine("eipa oo karkausvuosi");
+
+            
+
+        }
+    }
+}
